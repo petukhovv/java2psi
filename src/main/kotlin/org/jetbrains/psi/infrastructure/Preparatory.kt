bb2e662cb6d9c7bd7fea84ae0537cbe9ae384266
Victor Petukhov <i@victor.am>
2018-02-10 21:10:19 +0300

    Save psi as json implemented

---
+++ src/main/kotlin/org/jetbrains/psi/infrastructure/Preparatory.kt
@@ -0,0 +1,32 @@
+package org.jetbrains.psi.infrastructure
+
+import com.intellij.lang.Language
+import com.intellij.lang.LanguageParserDefinitions
+import com.intellij.lang.java.JavaParserDefinition
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.roots.ProjectFileIndex
+import com.intellij.openapi.roots.ProjectRootManager
+import com.intellij.openapi.roots.impl.DirectoryIndex
+import com.intellij.openapi.roots.impl.DirectoryIndexImpl
+import com.intellij.openapi.roots.impl.ProjectFileIndexImpl
+import com.intellij.openapi.roots.impl.ProjectRootManagerImpl
+import com.intellij.openapi.util.Disposer
+import com.intellij.psi.impl.JavaPsiImplementationHelper
+import com.intellij.psi.impl.JavaPsiImplementationHelperImpl
+import org.intellij.grammar.LightPsi
+
+abstract class Preparatory {
+    companion object {
+        fun prepare(language: Language): Project {
+            val project = LightPsi.Init.initAppAndProject(Disposer.newDisposable())
+
+            project.registerService(JavaPsiImplementationHelper::class.java, JavaPsiImplementationHelperImpl::class.java)
+            project.registerService(ProjectRootManager::class.java, ProjectRootManagerImpl::class.java)
+            project.registerService(ProjectFileIndex::class.java, ProjectFileIndexImpl::class.java)
+            project.registerService(DirectoryIndex::class.java, DirectoryIndexImpl::class.java)
+            LightPsi.Init.addKeyedExtension(LanguageParserDefinitions.INSTANCE, language, JavaParserDefinition(), project)
+
+            return project
+        }
+    }
+}
---
+++ src/main/kotlin/org/jetbrains/psi/io/FilesWriter.kt
@@ -0,0 +1,17 @@
+package org.jetbrains.psi.io
+
+import com.google.gson.Gson
+import com.google.gson.JsonArray
+import java.io.File
+
+class FilesWriter {
+    companion object {
+        fun write(inputDir: String, outputDir: String, file: File, content: JsonArray) {
+            val relativePath = file.relativeTo(File(inputDir))
+            val outputPath = File("$outputDir/$relativePath.json")
+
+            File("$outputDir/${relativePath.parent ?: ""}").mkdirs()
+            outputPath.writeText(Gson().toJson(content))
+        }
+    }
+}
