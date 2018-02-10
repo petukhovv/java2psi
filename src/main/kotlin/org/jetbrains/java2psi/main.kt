140bf83447cb8a4d195101d9eb395c8d218881ff
Victor Petukhov <i@victor.am>
2018-02-10 21:39:10 +0300

    Project rename

--- src/main/kotlin/org/jetbrains/java2psi/main.kt
+++ src/main/kotlin/org/jetbrains/java2psi/main.kt
@@ -1,18 +1,18 @@
-package org.jetbrains.psi
+package org.jetbrains.java2psi
 
 import com.intellij.ide.highlighter.JavaFileType
 import com.intellij.psi.PsiFileFactory
 import com.xenomachina.argparser.ArgParser
-import org.jetbrains.psi.infrastructure.Preparatory
-import org.jetbrains.psi.io.FilesReader
-import org.jetbrains.psi.io.FilesWriter
+import org.jetbrains.java2psi.infrastructure.Preparatory
+import org.jetbrains.java2psi.io.FilesReader
+import org.jetbrains.java2psi.io.FilesWriter
 import java.io.File
 
 
 fun main(args : Array<String>) {
     val parser = ArgParser(args)
-    val sourcesDir by parser.storing("-i", "--input", help="path to dir with java source code files for psi extraction")
-    val psiDir by parser.storing("-o", "--output", help="path to dir in which will be written psi trees")
+    val sourcesDir by parser.storing("-i", "--input", help="path to dir with java source code files to java2psi extraction")
+    val psiDir by parser.storing("-o", "--output", help="path to dir in which will be written java2psi trees")
 
     val language = JavaFileType.INSTANCE.language
     val project = Preparatory.prepare(language)
